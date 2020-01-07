e7057483cb7f39c4f01834b3c3101aed1e9b7d02
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-01-07 07:30:13 -0800

    saving this

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::io; // provides useful inputting features
+use rand::Rng; // provides random number generation
 
 // entry function
 fn main() {
     println!("Guess the number.");
 
     // variables immutable by default
-    let mut guess = String::new(); // makes a new string
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
+
+    let mut guess = String::new(); // makes a new mutable string
+
+    // queries for response
+    // note it passes a reference, not the value itself
     io::stdin().read_line(&mut guess).expect("Failed to read line."); // will crash the program if there's an err type returned
-    println!("You guessed: {}", guess);
+
+    println!("You guessed {}", guess);
 }
