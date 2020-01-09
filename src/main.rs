30c3d8c5152012b0c90757846dabb725331947b7
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-01-08 21:04:09 -0800

    doing webgpu instead

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,5 @@
-use std::env;
+use wgpu;
 
-struct Constants {
-
-    width: usize,
-    height: usize,
-
-    wrap: bool,
-    ticks:u32,
-
-    dead: u8,
-    live: u8,
-
-}
-
-const CONSTANTS : Constants = Constants {
-    width: 50,
-    height: 50,
-
-    wrap: true,
-    ticks: 10,
-
-    dead: 0b0000000,
-    live: 0b0000001,
-};
-
-fn main() {
-    let mut board: [[u8; CONSTANTS.width]; CONSTANTS.height] =
-        [[CONSTANTS.dead; CONSTANTS.width]; CONSTANTS.height];
-
-    let mut index = 0;
-    loop {
-        update(&board);
-
-        // print the board
-
-        index += 1;
-        if index > CONSTANTS.ticks {
-            break;
-        }
-    }
-}
-
-fn update(board: &[[u8; CONSTANTS.width]; CONSTANTS.height]){
+fn main(){
     //
 }
