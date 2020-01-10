07ad21a1f9d05c8dc398f1b464e3b8a29b0a516e
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-01-09 21:39:12 -0800

    modderme got an example to work, but it is only running on his machine

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use wgpu;
 use winit::{
+    dpi,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
     window::Window,
-    dpi,
+    window::WindowBuilder,
 };
 
 struct WindowConfig {
-
     min_width: u16,
     min_height: u16,
     max_width: u16,
@@ -17,25 +16,101 @@ struct WindowConfig {
     visible: bool,
     title: String,
     always_on_top: bool,
-
     // TODO add more elements to the config
-
 }
 
-fn main(){
+fn main() {
     let event_loop = EventLoop::new();
-    let window: Window = build_window(&event_loop, &WindowConfig {
-        min_width: 600,
-        min_height: 600,
-        max_width: 2000,
-        max_height: 2000,
-        resizeable: true,
-        visible: true,
-        title: "test title".to_string(),
-        always_on_top: false,
+    let window: Window = build_window(
+        &event_loop,
+        &WindowConfig {
+            min_width: 600,
+            min_height: 600,
+            max_width: 2000,
+            max_height: 2000,
+            resizeable: true,
+            visible: true,
+            title: "test title".to_string(),
+            always_on_top: false,
+        },
+    );
+    let size = window.inner_size();
+
+    let surface = wgpu::Surface::create(&window);
+
+    let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::Default,
+        backends: wgpu::BackendBit::PRIMARY,
+    }).unwrap();
+
+    let (device, mut queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+        extensions: wgpu::Extensions {
+            anisotropic_filtering: false,
+        },
+        limits: wgpu::Limits::default(),
     });
 
-    event_loop.run(move | event, _, control_flow | {
+    let vs = include_bytes!("shader.vert.spv");
+    let vs_module =
+        device.create_shader_module(&wgpu::read_spirv(std::io::Cursor::new(&vs[..])).unwrap());
+
+    let fs = include_bytes!("shader.frag.spv");
+    let fs_module =
+        device.create_shader_module(&wgpu::read_spirv(std::io::Cursor::new(&fs[..])).unwrap());
+
+    let bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor { bindings: &[] });
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &bind_group_layout,
+        bindings: &[],
+    });
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        bind_group_layouts: &[&bind_group_layout],
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        layout: &pipeline_layout,
+        vertex_stage: wgpu::ProgrammableStageDescriptor {
+            module: &vs_module,
+            entry_point: "main",
+        },
+        fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+            module: &fs_module,
+            entry_point: "main",
+        }),
+        rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: wgpu::CullMode::None,
+            depth_bias: 0,
+            depth_bias_slope_scale: 0.0,
+            depth_bias_clamp: 0.0,
+        }),
+        primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+        color_states: &[wgpu::ColorStateDescriptor {
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            color_blend: wgpu::BlendDescriptor::REPLACE,
+            alpha_blend: wgpu::BlendDescriptor::REPLACE,
+            write_mask: wgpu::ColorWrite::ALL,
+        }],
+        depth_stencil_state: None,
+        index_format: wgpu::IndexFormat::Uint16,
+        vertex_buffers: &[],
+        sample_count: 1,
+        sample_mask: !0,
+        alpha_to_coverage_enabled: false,
+    });
+
+    let mut swap_chain = device.create_swap_chain(
+        &surface,
+        &wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width as u32,
+            height: size.height as u32,
+            present_mode: wgpu::PresentMode::Vsync,
+        },
+    );
+    event_loop.run(move |event, _, control_flow| {
         // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
         // dispatched any events. This is ideal for games and similar applications.
         *control_flow = ControlFlow::Poll;
@@ -49,21 +124,35 @@ fn main(){
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
-            } => {
-                *control_flow = ControlFlow::Exit
-            },
+            } => *control_flow = ControlFlow::Exit,
             Event::MainEventsCleared => {
                 // Application update code.
                 // Queue a RedrawRequested event.
                 window.request_redraw();
-            },
+            }
             Event::RedrawRequested(_) => {
-                // Redraw the application.
-                // It's preferrable to render in this event rather than in MainEventsCleared, since
-                // rendering in here allows the program to gracefully handle redraws requested
-                // by the OS.
-            },
-            _ => ()
+                let frame = swap_chain.get_next_texture();
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                            attachment: &frame.view,
+                            resolve_target: None,
+                            load_op: wgpu::LoadOp::Clear,
+                            store_op: wgpu::StoreOp::Store,
+                            clear_color: wgpu::Color::GREEN,
+                        }],
+                        depth_stencil_attachment: None,
+                    });
+                    rpass.set_pipeline(&render_pipeline);
+                    rpass.set_bind_group(0, &bind_group, &[]);
+                    rpass.draw(0..3, 0..1);
+                }
+
+                queue.submit(&[encoder.finish()]);
+            }
+            _ => (),
         }
     });
 }
